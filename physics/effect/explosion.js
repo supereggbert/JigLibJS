9b92c5c7c65edf518b44266faaf2263e99631fc5
Jim Sangwine <jim@competa.com>
2010-08-24 18:15:15 +0200

    added Wind effect

--- physics/effect/explosion.js
+++ physics/effect/explosion.js
@@ -3,31 +3,42 @@
 	/**
 	 * @author Jim Sangwine
 	 * 
-	 * This effect has a radius within which it will either attract or repel bodies depending on the defined force 
-	 * (positive values attract, negative repel) and their distance (the closer the object, the stronger the effect).
+	 * This effect has a radius within which it will repel bodies depending on the defined force 
+	 * and their distance (the closer the object, the stronger the effect). 
 	 * 
-	 * This effect will be applied continuously as long as this.enabled == true
+	 * This effect will only be applied during a single cycle of the PhysicsSystem, imparting a sudden impulse.
 	 * 
 	 * This effect can either be placed at an arbitrary location in the scene, or it can be attached to a parent object.
 	 * 
 	 * @param {Array}	location	vector array in the format [x,y,z]
 	 * @param {Number}	radius		radius of effect - at [radius] distance, gravity effect will be 0
-	 * @param {Number}	force		the force of gravity at 0 distance (impulse will be force/distance)
+	 * @param {Number}	force		the force of the explosion at 0 distance (impulse will be force/distance)
 	 * @param {Object}	parent		optional - a RigidBody that the gravitational field will follow - excluded from effect 
 	 **/
-	var GravityField=function(_location, _radius, _force, _parent) {
+	var Explosion=function(_location, _radius, _force, _parent) {
 		this.super();
 		this.location=_location;
 		this.radius=_radius;
 		this.force=_force;
 		if (_parent) this.parent=_parent;
+		// set to NOT fire instantly...
+		this.enabled = false;
 	};
-	jigLib.extends(GravityField,jigLib.JEffect);
+	jigLib.extends(Explosion,jigLib.JEffect);
 
-	GravityField.prototype.location = null;
-	GravityField.prototype.radius = null;
-	GravityField.prototype.force = null;
-	GravityField.prototype.parent = null;
+	Explosion.prototype.location = null;
+	Explosion.prototype.radius = null;
+	Explosion.prototype.force = null;
+	Explosion.prototype.parent = null;
+
+	/**
+	 * Sets the effect to fire the next time Apply() is called
+	 * 
+	 * @returns
+	 */
+	Explosion.prototype.explode = function() {
+		this.enabled = true;
+	};
 	
 	/**
 	 * Applies the effect to the relevant bodies.
@@ -35,16 +46,18 @@
 	 * 
 	 * @returns
 	 */
-	GravityField.prototype.Apply = function() {
+	Explosion.prototype.Apply = function() {
+		this.enabled = false;
+		
 		var bodies=jigLib.PhysicsSystem.getInstance().get_bodies();
-		var i=bodies.length-1;
+		var i=bodies.length;
 		var curBody, distance, force, forceV;
 		
 		if (this.parent)
 			this.location = this.parent.get_position();
 		
 		this._affectedBodies=[];
-		do {
+		while(i--) {
 			curBody=bodies[i];
 			if (!curBody.get_movable() || (this.parent && curBody == this.parent)) continue;
 
@@ -54,11 +67,10 @@
 				forceV=Vector3DUtil.subtract(curBody.get_position(), this.location);
 				force=(1-(distance / this.radius)) * this.force;
 				Vector3DUtil.scaleBy(forceV, force);
-				Vector3DUtil.negate(forceV);
 				curBody.addWorldForce(forceV, this.location);
 			}
-		} while(i--);
+		}
 	};
 	
-	jigLib.GravityField=GravityField;
+	jigLib.Explosion=Explosion;
 })(jigLib);
---
+++ physics/effect/wind.js
@@ -0,0 +1,55 @@
+(function(jigLib){
+	var Vector3DUtil=jigLib.Vector3DUtil;
+	/**
+	 * @author Jim Sangwine
+	 * 
+	 * This effect has global influence - All objects that are movable in the scene will be affected, apart from those
+	 * added to the exclusions array. 
+	 * 
+	 * This effect will be applied continuously as long as this.enabled == true.
+	 * 
+	 * This effect accepts a vector to define it's direction of influence.
+	 * 
+	 * @param {Array}	direction	vector array in the format [x,y,z] defines the force of the effect in each direction
+	 * @param {Array}	exclusions	optional - a list of bodies that should be excluded from the effect 
+	 **/
+	var Wind=function(_direction, _exclusions) {
+		this.super();
+		this.direction=_direction;
+		if (_exclusions) this.exclusions=_exclusions;
+	};
+	jigLib.extends(Wind,jigLib.JEffect);
+
+	Wind.prototype.direction = null;
+	Wind.prototype.exclusions = [];
+	
+	/**
+	 * Searches exclusions for a given body
+	 */
+	Wind.prototype.isExcluded = function(body) {
+		var i=this.exclusions.length;
+		while (i--) { if (this.exclusions[i] == body) return true; }
+		return false;
+	};
+	
+	/**
+	 * Applies the effect to the relevant bodies.
+	 * Typically called by PhysicsSystem.handleAllEffects. 
+	 * 
+	 * @returns
+	 */
+	Wind.prototype.Apply = function() {
+		var bodies=jigLib.PhysicsSystem.getInstance().get_bodies();
+		var i=bodies.length;
+		var curBody;
+		
+		this._affectedBodies=[];
+		while(i--) {
+			curBody=bodies[i];
+			if (!curBody.get_movable() || this.isExcluded(curBody)) continue;
+			curBody.addWorldForce(this.direction, [0,0,0]);
+		}
+	};
+	
+	jigLib.Wind=Wind;
+})(jigLib);
